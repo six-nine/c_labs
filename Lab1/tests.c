6d2309f818541aa8a59430fc0d5c26c91dfc7106
Вадим Козлов <vadimkozlov@MacBook-Air-Vadim.local>
2021-03-10 11:06:11 +0300

    lint and valgrind added

--- Lab1/tests.c
+++ Lab1/tests.c
@@ -1,22 +1,23 @@
 #include <assert.h>
 #include <stdio.h>
+
 #include "main.h"
 
 void test() {
-	assert(is_perfect(6) == 1);
-	assert(is_perfect(1) == 0);
-	assert(is_perfect(8128) == 1);
-	assert(is_perfect(496) == 1);
-	assert(is_perfect(105) == 0);
-	assert(is_perfect(106) == 0);
-	assert(is_perfect(1000000) == 0);
-	assert(is_perfect(-5) == 0);
-	assert(is_perfect(199999) == 0);
-	printf("All tests are passed.\n");
+  assert(is_perfect(6) == 1);
+  assert(is_perfect(1) == 0);
+  assert(is_perfect(8128) == 1);
+  assert(is_perfect(496) == 1);
+  assert(is_perfect(105) == 0);
+  assert(is_perfect(106) == 0);
+  assert(is_perfect(1000000) == 0);
+  assert(is_perfect(-5) == 0);
+  assert(is_perfect(199999) == 0);
+  printf("All tests are passed.\n");
 }
 #undef main
 
 int main() {
-	test();
-	return 0;
+  test();
+  return 0;
 }
