1651d42476d2f138fd7bfccadb58d5524edbdeef
Вадим Козлов <vadimkozlov@MacBook-Air-Vadim.local>
2021-03-17 11:40:32 +0300

    guards added

--- Lab2(2.1)/main.h
+++ Lab2(2.1)/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void get_validated_int(int *variable, int min_value, int max_value);
 void get_validated_double(double *variable, double min_value, double max_value,
                           char *message);
@@ -8,3 +11,5 @@ double get_volume(double radius);
 double get_tetradeon(double radius);
 double get_cross_sectional_area(double radius, double k);
 void get_info();
+
+#endif
