f3969f3968db054adc08a6da330d1938a8c0425c
Вадим Козлов <vadimkozlov@MacBook-Air-Vadim.local>
2021-05-20 22:19:13 +0300

    fixes

--- Lab7/tests.cpp
+++ Lab7/tests.cpp
@@ -14,5 +14,8 @@ int main(){
   assert(app1.get_marital_status() == app_from_csv.get_marital_status());
   assert(app1.get_gender() == app_from_csv.get_gender());
   assert(app1.get_year_of_birth() == app_from_csv.get_year_of_birth());
+  assert(app1.min_salary() == app_from_csv.min_salary());
+  assert(app1.to_string() == app_from_csv.to_string());
+  assert(app1.to_csv() == "Ivanov,Ivan,Ivanovich,2010,1,0,0,73673682361742687,3,1600");
   return 0;
 }
