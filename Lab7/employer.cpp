5474d20951440e241f0bd2e04226e89d5d3bce08
Вадим Козлов <vadimkozlov@MacBook-Air-Vadim.local>
2021-05-21 18:24:07 +0300

    Tests update

--- Lab7/employer.cpp
+++ Lab7/employer.cpp
@@ -15,8 +15,8 @@ employer::employer(int city_id, int min_salary, int max_year_of_birth)
 bool employer::suits(applicant appl) {
   return
       appl.get_city() == ideal_candidate.get_city() &&
-          appl.min_salary() <= ideal_candidate.min_salary() &&
-          appl.get_year_of_birth() <= ideal_candidate.get_year_of_birth();
+      appl.min_salary() <= ideal_candidate.min_salary() &&
+      appl.get_year_of_birth() <= ideal_candidate.get_year_of_birth();
 }
 std::string employer::to_csv() {
   return ideal_candidate.to_csv();
